d9819fdb4d4647bc416765ccb6557a6666c615e6
nyLiao <39255546+nyLiao@users.noreply.github.com>
2024-02-25 00:48:17 +0800

    Variable layer example

--- pyg_spectral/nn/conv/fix_sumadj.py
+++ pyg_spectral/nn/conv/fix_sumadj.py
@@ -1,3 +1,6 @@
+from typing import Tuple, Union
+
+import numpy as np
 import torch
 from torch import Tensor
 
@@ -7,40 +10,62 @@ from torch_geometric.typing import Adj, OptTensor
 from torch_geometric.utils import spmm, dropout_edge
 
 
-def gen_theta(K: int, scheme: str, decay: float = None) -> Tensor:
+def gen_theta(K: int, scheme: str, alpha: float = None) -> Tensor:
     r"""Generate list of hop parameters based on given scheme.
 
     Args:
-        K (int): Number of iterations :math:`K`.
+        K (int): Number of hops.
         scheme (str): Method to generate parameters.
-            * `sgc`: only K-hop, :math:`\mathbf{H} = \mathbf{A}^K \mathbf{X}`.
-            * 'appnp': Personalized PageRank, :math:`\mathbf{H} = \sum_{k=0}^K \alpha (1 - \alpha)^k \mathbf{A}^k \mathbf{X}`.
-            * `ssgc`: Monomial, :math:`\mathbf{H} = \frac{1}{K} \sum_{k=1}^K\left((1-\alpha)\mathbf{A}^k + \alpha \mathbf{I}\right) \mathbf{X}`.
-            * 'hk': Heat Kernel, :math:`\mathbf{H} = \sum_{k=0}^K e^{-\alpha k} \mathbf{A}^k \mathbf{X}`.
-        decay: Decay factor among hops.
-            * 'sgc': not used.
-            * 'appnp': :math:`decay \in [0, 1]`.
-            * 'ssgc'::math:`decay \in [0, 1]`.
-            * 'hk': :math:`decay > 0`.
+            - `khop`: K-hop, :math:`\mathbf{H} = \mathbf{A}^K \mathbf{X}`.
+            - 'appr': Approximate PPR, :math:`\mathbf{H} = \sum_{k=0}^K \alpha (1 - \alpha)^k \mathbf{A}^k \mathbf{X}`.
+            - 'nappr': Negative PPR, :math:`\mathbf{H} = \sum_{k=0}^K \alpha^k \mathbf{A}^k \mathbf{X}`.
+            - `mono`: Monomial, :math:`\mathbf{H} = \frac{1}{K} \sum_{k=1}^K\left((1-\alpha)\mathbf{A}^k + \alpha \mathbf{I}\right) \mathbf{X}`.
+            - 'hk': Heat Kernel, :math:`\mathbf{H} = \sum_{k=0}^K e^{-\alpha k} \mathbf{A}^k \mathbf{X}`.
+            - 'uniform': Random uniform distribution.
+            - 'normal': Random normal distribution.
+        alpha (float, optional): Hyperparameter for the scheme.
+            - 'khop': Only the alpha-hop, :math:`\alpha \in [0, K]`.
+            - 'appr': Decay factor, :math:`\alpha \in [0, 1]`.
+            - 'nappr': Decay factor, :math:`\alpha \in [-1, 1]`.
+            - 'mono': Decay factor, :math:`\alpha \in [0, 1]`.
+            - 'hk': Decay factor, :math:`\alpha > 0`.
+            - 'uniform': Distribution bound.
+            - 'normal': Distribution variance.
 
     Returns:
         theta (Tensor): List of hop parameters.
     """
-    assert K > 1, 'K should be greater than 1'
-    if scheme == 'sgc':
-        return torch.tensor([0.0] * (K - 1) + [1.0])
-    elif scheme == 'appnp':
-        decay = decay if decay is not None else 0.5
-        return decay * (1 - decay) ** torch.arange(K)
-    elif scheme == 'ssgc':
-        decay = decay if decay is not None else 0.5
-        theta = torch.zeros(K)
-        theta[0] = decay
-        theta[1:] = (1 - decay) / (K - 1)
+    assert K > 0, 'K should be a positive integer'
+    if scheme == 'khop':
+        alpha = alpha if alpha is not None else K
+        theta = torch.zeros(K+1)
+        theta[alpha] = 1
+        return theta
+    elif scheme == 'appr':
+        alpha = alpha if alpha is not None else 0.5
+        # theta[-1] = (1 - alpha) ** K
+        return alpha * (1 - alpha) ** torch.arange(K+1)
+    elif scheme == 'nappr':
+        alpha = alpha if alpha is not None else 0.5
+        theta = alpha ** torch.arange(K+1)
+        return theta/torch.norm(theta, p=1)
+    elif scheme == 'mono':
+        alpha = alpha if alpha is not None else 0.5
+        theta = torch.zeros(K+1)
+        theta[0] = alpha
+        theta[1:] = (1 - alpha) / K
         return theta
     elif scheme == 'hk':
-        decay = decay if decay is not None else 1.0
-        return torch.exp(-decay * torch.arange(K))
+        alpha = alpha if alpha is not None else 1.0
+        return torch.exp(-alpha * torch.arange(K+1))
+    elif scheme == 'uniform':
+        alpha = alpha if alpha is not None else np.sqrt(3/(K+1))
+        theta = torch.rand(K+1) * 2 * alpha - alpha
+        return theta/torch.norm(theta, p=1)
+    elif scheme == 'normal':
+        alpha = alpha if alpha is not None else 1.0
+        theta = torch.randn(K+1) * alpha
+        return theta/torch.norm(theta, p=1)
     else:
         raise NotImplementedError()
 
@@ -49,16 +74,31 @@ class FixSumAdj(MessagePassing):
     r"""Summation of hops of adj-based propagations with fixed parameters.
     Similar to `pyg.nn.conv.APPNP`
 
+    Covers:
+        - `khop`: SGC, DAGNN
+        - 'appr': APPNP
+        - 'mono': S2GC
+        - 'hk': GDC, AGP
+
     Args:
         theta (Tensor): List of hop parameters.
         K (int, optional): Number of iterations :math:`K`. If K=0 then infer from p.
         dropout (float, optional): Edge dropout. Defaults to 0.
+
+    Shapes:
+        - **input:**
+          node features :math:`(|\mathcal{V}|, F)`,
+          edge indices :math:`(2, |\mathcal{E}|)`,
+          edge weights :math:`(|\mathcal{E}|)` *(optional)*
+        - **output:** node features :math:`(|\mathcal{V}|, F)`
     """
-    def __init__(self, theta: Tensor, K: int = 0,
+    def __init__(self, theta: Union[Tensor, Tuple[str, float]], K: int = 0,
                  dropout: float = 0., **kwargs):
         kwargs.setdefault('aggr', 'add')
-        super().__init__(**kwargs)
+        super(FixSumAdj, self).__init__(**kwargs)
 
+        if isinstance(theta, tuple):
+            theta = gen_theta(K, *theta)
         self.theta = theta
         self.K = K if K > 0 else len(theta)
         self.dropout = dropout
@@ -74,7 +114,7 @@ class FixSumAdj(MessagePassing):
             h = torch.zeros_like(x)
         else:
             h = x * self.theta[0]
-        for k in range(1, self.K):
+        for k in range(1, self.K+1):
             # Edge dropout
             edge_index, edge_mask = dropout_edge(edge_index, p=self.dropout, training=self.training)
             if edge_weight is not None:
@@ -94,32 +134,37 @@ class FixSumAdj(MessagePassing):
         return spmm(adj_t, x, reduce=self.aggr)
 
     def __repr__(self) -> str:
-        return f'{self.__class__.__name__}(K={self.K}, decay={self.theta[0]:f})'
+        return f'{self.__class__.__name__}(K={self.K}, alpha={self.theta[0]:f})'
 
 
-class FixLinSumAdj(MessagePassing):
+class FixLinSumAdj(FixSumAdj):
     r"""Summation of hops of adj-based propagations with fixed parameters, then
     perform linear transformation. Similar to `pyg.nn.conv.SSGConv`
 
-    Args:
-        theta (Tensor): List of hop parameters.
-        K (int, optional): Number of iterations :math:`K`. If K=0 then infer from p.
-        dropout (float, optional): Edge dropout. Defaults to 0.
+    Covers:
+        * `khop`: SGC, DAGNN
+        * 'mono': S2GC
+
+    Shapes:
+        - **input:**
+          node features :math:`(|\mathcal{V}|, F_{in})`,
+          edge indices :math:`(2, |\mathcal{E}|)`,
+          edge weights :math:`(|\mathcal{E}|)` *(optional)*
+        - **output:**
+          node features :math:`(|\mathcal{V}|, F_{out})`
     """
     def __init__(self, in_channels: int, out_channels: int,
-                 theta: Tensor, K: int = 0,
+                 theta: Union[Tensor, Tuple[str, float]], K: int = 0,
                  dropout: float = 0., bias: bool = True, **kwargs):
         kwargs.setdefault('aggr', 'add')
-        super().__init__(**kwargs)
-
-        self.theta = theta
-        self.K = K if K > 0 else len(theta)
-        self.dropout = dropout
+        super(FixLinSumAdj, self).__init__(theta, K, dropout, **kwargs)
 
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.lin = Linear(in_channels, out_channels, bias=bias)
-        self.reset_parameters()
+
+        if self.__class__ == FixLinSumAdj:
+            self.reset_parameters()
 
     def reset_parameters(self):
         super().reset_parameters()
@@ -132,29 +177,9 @@ class FixLinSumAdj(MessagePassing):
         edge_weight: OptTensor = None,
     ) -> Tensor:
 
-        if self.theta[0] == 0:
-            h = torch.zeros_like(x)
-        else:
-            h = x * self.theta[0]
-        for k in range(1, self.K):
-            # Edge dropout
-            edge_index, edge_mask = dropout_edge(edge_index, p=self.dropout, training=self.training)
-            if edge_weight is not None:
-                edge_weight = edge_weight[edge_mask]
-
-            # propagate_type: (x: Tensor, edge_weight: OptTensor)
-            x = self.propagate(edge_index, x=x, edge_weight=edge_weight)
-            if self.theta[k] != 0:
-                h += x * self.theta[k]
-
+        h = super().forward(x, edge_index, edge_weight)
         return self.lin(h)
-
-    def message(self, x_j: Tensor, edge_weight: OptTensor) -> Tensor:
-        return x_j if edge_weight is None else edge_weight.view(-1, 1) * x_j
-
-    def message_and_aggregate(self, adj_t: Adj, x: Tensor) -> Tensor:
-        return spmm(adj_t, x, reduce=self.aggr)
 
     def __repr__(self) -> str:
         return (f'{self.__class__.__name__}({self.in_channels}, '
-                f'{self.out_channels}, K={self.K}, decay={self.theta[0]:f})')
+                f'{self.out_channels}, K={self.K}, alpha={self.theta[0]:f})')
---
+++ pyg_spectral/nn/conv/var_sumadj.py
@@ -0,0 +1,129 @@
+from typing import Tuple, Union
+
+import torch
+from torch import Tensor
+
+from torch_geometric.nn.conv import MessagePassing
+from torch_geometric.nn.dense.linear import Linear
+from torch_geometric.typing import Adj, OptTensor
+from torch_geometric.utils import spmm, dropout_edge
+
+from pyg_spectral.nn.conv.fix_sumadj import gen_theta
+
+
+class VarSumAdj(MessagePassing):
+    r"""Summation of hops of adj-based propagation with learnable parameters.
+    Similar to [GPRGNN](https://github.com/jianhao2016/GPRGNN/blob/master/src/GNN_models.py)
+
+    Covers:
+        - 'appr': GPRGNN
+
+    Args:
+        theta (Tensor): List of initial hop parameters.
+        K (int, optional): Number of iterations :math:`K`. If K=0 then infer from p.
+        dropout (float, optional): Edge dropout. Defaults to 0.
+
+    Shapes:
+        - **input:**
+          node features :math:`(|\mathcal{V}|, F)`,
+          edge indices :math:`(2, |\mathcal{E}|)`,
+          edge weights :math:`(|\mathcal{E}|)` *(optional)*
+        - **output:** node features :math:`(|\mathcal{V}|, F)`
+    """
+    def __init__(self, theta: Union[Tensor, Tuple[str, float]], K: int = 0,
+                 dropout: float = 0., **kwargs):
+        kwargs.setdefault('aggr', 'add')
+        super(VarSumAdj, self).__init__(**kwargs)
+
+        if isinstance(theta, tuple):
+            theta = gen_theta(K, *theta)
+        self.theta_init = theta
+        self.theta = torch.nn.Parameter(theta)
+        self.K = K if K > 0 else len(theta)
+        self.dropout = dropout
+
+        if self.__class__ == VarSumAdj:
+            self.reset_parameters()
+
+    def reset_parameters(self):
+        super().reset_parameters()
+        self.theta.data = self.theta_init
+
+    def forward(
+        self,
+        x: Tensor,
+        edge_index: Adj,
+        edge_weight: OptTensor = None,
+    ) -> Tensor:
+
+        if self.theta[0] == 0:
+            h = torch.zeros_like(x)
+        else:
+            h = x * self.theta[0]
+        for k in range(1, self.K+1):
+            # Edge dropout
+            edge_index, edge_mask = dropout_edge(edge_index, p=self.dropout, training=self.training)
+            if edge_weight is not None:
+                edge_weight = edge_weight[edge_mask]
+
+            # propagate_type: (x: Tensor, edge_weight: OptTensor)
+            x = self.propagate(edge_index, x=x, edge_weight=edge_weight)
+            if self.theta[k] != 0:
+                h += x * self.theta[k]
+
+        return h
+
+    def message(self, x_j: Tensor, edge_weight: OptTensor) -> Tensor:
+        return x_j if edge_weight is None else edge_weight.view(-1, 1) * x_j
+
+    def message_and_aggregate(self, adj_t: Adj, x: Tensor) -> Tensor:
+        return spmm(adj_t, x, reduce=self.aggr)
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}(K={self.K}, alpha={self.theta[0]:f})'
+
+
+class VarLinSumAdj(VarSumAdj):
+    r"""Summation of hops of adj-based propagation with learnable parameters, then
+    perform linear transformation.
+
+    Covers:
+
+    Shapes:
+        - **input:**
+          node features :math:`(|\mathcal{V}|, F_{in})`,
+          edge indices :math:`(2, |\mathcal{E}|)`,
+          edge weights :math:`(|\mathcal{E}|)` *(optional)*
+        - **output:**
+          node features :math:`(|\mathcal{V}|, F_{out})`
+    """
+    def __init__(self, in_channels: int, out_channels: int,
+                 theta: Union[Tensor, Tuple[str, float]], K: int = 0,
+                 dropout: float = 0., bias: bool = True, **kwargs):
+        kwargs.setdefault('aggr', 'add')
+        super(VarLinSumAdj, self).__init__(theta, K, dropout, **kwargs)
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.lin = Linear(in_channels, out_channels, bias=bias)
+
+        if self.__class__ == VarLinSumAdj:
+            self.reset_parameters()
+
+    def reset_parameters(self):
+        super().reset_parameters()
+        self.lin.reset_parameters()
+
+    def forward(
+        self,
+        x: Tensor,
+        edge_index: Adj,
+        edge_weight: OptTensor = None,
+    ) -> Tensor:
+
+        h = super().forward(x, edge_index, edge_weight)
+        return self.lin(h)
+
+    def __repr__(self) -> str:
+        return (f'{self.__class__.__name__}({self.in_channels}, '
+                f'{self.out_channels}, K={self.K}, alpha={self.theta[0]:f})')
