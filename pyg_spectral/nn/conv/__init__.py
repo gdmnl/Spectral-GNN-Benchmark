d95ec395d9639c4a82dabfd8fbb5ff2447ba86a5
hyLiu-777 <haoyu.liu@ntu.edu.sg>
2024-03-25 16:09:41 +0800

    BernConv

--- pyg_spectral/nn/conv/__init__.py
+++ pyg_spectral/nn/conv/__init__.py
@@ -2,3 +2,4 @@ from .fix_sumadj import FixSumAdj, FixLinSumAdj
 from .var_sumadj import VarSumAdj, VarLinSumAdj
 from .cheb_base import ChebBase
 from .cheb_conv2 import ChebConv2
+from .bern_conv import BernConv
--- pyg_spectral/nn/conv/bern_conv.py
+++ pyg_spectral/nn/conv/bern_conv.py
@@ -1,4 +1,5 @@
-"""Referencing https://github.com/ivam-he/BernNet"""
+from typing import Final
+
 from typing import Optional, Tuple
 from torch_geometric.typing import Adj, OptTensor, PairTensor
 import math
@@ -17,42 +18,86 @@ from scipy.special import comb
 
 class BernConv(MessagePassing):
 
-    def __init__(self, K, **kwargs):
+    r"""Convolutional layer with Bernstein Polynomials.
 
+    Args:
+        K (int, optional): Number of iterations :math:`K`.
+    """
+
+    supports_edge_weight: Final[bool] = False
+    supports_batch: Final[bool] = False
+    supports_decouple: Final[bool] = True
+    supports_norm_batch: Final[bool] = False
+
+    def __init__(self, K: int = 0, cached: bool = True, **kwargs):
         kwargs.setdefault('aggr', 'add')
         super(BernConv, self).__init__(**kwargs)
-        assert K > 0
+
         self.K = K
-        self.temp = Parameter(torch.Tensor(self.K+1))
+        self.theta = Parameter(torch.Tensor(self.K+1))
+        self.cached = cached
+        self._cached_adj_t = None
         self.reset_parameters()
     
     def reset_parameters(self):
-        self.temp.data.fill_(0.0)
-        self.temp.data[0]=1.0
+        self.theta.data.fill_(0.0)
+        self.theta.data[0]=1.0
+        self._cached_adj_t = None
 
-    def forward(self,x,edge_index,edge_weight=None):
+    def forward(self,
+                x: Tensor,
+                edge_index: Adj,
+                edge_weight: OptTensor=None,
+        ) -> Tensor:
 
-        #L=I-D^(-0.5)AD^(-0.5)
-        edge_index1, norm1 = get_laplacian(edge_index, edge_weight, normalization=True, dtype=x.dtype, num_nodes=x.size(self.node_dim))
+        cache = self._cached_adj_t
+        if cache is None:
+            if isinstance(edge_index, SparseTensor):
+                # A_norm -> L_norm
+                edge_index = get_laplacian(
+                    edge_index,
+                    normalization=True,
+                    dtype=x.dtype)
+                # L_norm -> L_norm - I
+                diag = edge_index.get_diag()
+                edge_index.set_diag(diag - 1.0)
+                adj_t = edge_index
 
-        #2I-L
-        edge_index2, norm2 = add_self_loops(edge_index1,-norm1, fill_value=2., num_nodes=x.size(self.node_dim))
+            else:
+                # A_norm -> L_norm
+                edge_index, edge_weight = get_laplacian(
+                    edge_index, edge_weight,
+                    normalization=True,
+                    dtype=x.dtype,
+                    num_nodes=x.size(self.node_dim))
+                # L_norm -> L_norm - I
+                edge_index, edge_weight = add_self_loops(
+                    edge_index, edge_weight,
+                    fill_value=-1.0,
+                    num_nodes=x.size(self.node_dim))
+                data = Data(edge_index=edge_index, edge_attr=edge_weight)
+                data.num_nodes = x.size(self.node_dim)
+                adj_t = T.ToSparseTensor(remove_edge_index=True)(data).adj_t
 
-        tmp=[]
-        tmp.append(x)
+            if self.cached:
+                self._cached_adj_t = adj_t
+        else:
+            adj_t = cache
+
+        tmp = [x]
         for i in range(self.K):
-                x=self.propagate(edge_index2,x=x,norm=norm2,size=None)
+                x = self.propagate(adj_t,x=x)
                 tmp.append(x)
 
-        out=(comb(self.K,0)/(2**self.K))*self.temp[0]*tmp[self.K]
+        out = (comb(self.K,0)/(2**self.K))*self.theta[0]*tmp[self.K]
 
         for i in range(self.K):
-                x=tmp[self.K-i-1]
-                x=self.propagate(edge_index1,x=x,norm=norm1,size=None)
+                x = tmp[self.K-i-1]
+                x = self.propagate(adj_t,x=x)
                 for j in range(i):
-                        x=self.propagate(edge_index1,x=x,norm=norm1,size=None)
+                        x = self.propagate(adj_t,x=x)
 
-                out=out+(comb(self.K,i+1)/(2**self.K))*self.temp[i+1]*x
+                out += (comb(self.K,i+1)/(2**self.K)) * self.theta[i+1] * x
 
         return out
 
@@ -60,5 +105,5 @@ class BernConv(MessagePassing):
         return norm.view(-1, 1) * x_j
 
     def __repr__(self):
-        return '{}({}, {}, K={}, normalization={})'.format(
-            self.__class__.__name__, self.K self.temp)
+        return '{}(K={})'.format(
+            self.__class__.__name__, self.K)
