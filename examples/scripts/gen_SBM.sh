0760d5ebb6c365f8aaf316772a29d53c4b87f7b2
ZulunZhu <812135011@qq.com>
2024-05-02 22:18:46 +0800

    add the synthetic dataset

--- examples/scripts/gen_SBM.sh
+++ examples/scripts/gen_SBM.sh
@@ -1,3 +1,3 @@
-g++ -std=c++17 ../gen_SBM/gen_SBM.cpp -o rd_dynamic
-./rd_dynamic -n 5000 -c 50 -ind 20 -outd 1
+g++ -std=c++17 ../gen_SBM/gen_SBM.cpp -o log
+./log -n 5000 -c 50 -ind 20 -outd 1
 python ../gen_SBM/gen_feature.py --n 5000
